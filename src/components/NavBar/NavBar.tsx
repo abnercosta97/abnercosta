a631c402a1d3279c9cdaf0490a0ce917cabbcba8
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2024-07-09 17:31:19 -0300

    fix: Ajustando responsividade e menu hamburguer

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,39 +1,109 @@
-import { AppBar, MenuItem, Toolbar, Typography, styled } from "@mui/material";
+import * as React from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Button,
+  MenuItem,
+  styled,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import Logo from "../../assets/images/logo.png";
 
+const pages = ["Sobre", "Skills", "Projetos"];
+
 const NavBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
   const StyledToobar = styled(Toolbar)(({ theme }) => ({
     display: "flex",
     justifyContent: "space-evenly",
     backgroundColor: theme.palette.background.default,
+    padding: "0",
+    margin: "0",
   }));
+
   const StyledImg = styled("img")(() => ({
     width: "80px",
   }));
 
   return (
-    <>
-      <AppBar position="absolute">
-        <StyledToobar>
-          <MenuItem>
+    <AppBar position="absolute">
+      <StyledToobar>
+        <Box sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}>
+          <a href="/">
             <StyledImg src={Logo} alt="Logo" />
-          </MenuItem>
+          </a>
+        </Box>
 
-          <MenuItem>
-            {" "}
-            <Typography color="secondary"> Sobre</Typography>{" "}
-          </MenuItem>
-          <MenuItem>
-            {" "}
-            <Typography color="secondary"> Skills</Typography>{" "}
-          </MenuItem>
-          <MenuItem>
-            {" "}
-            <Typography color="secondary"> Projetos</Typography>{" "}
-          </MenuItem>
-        </StyledToobar>
-      </AppBar>
-    </>
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="large"
+            aria-label="menu"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: "block", md: "none" },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+
+        <Box sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}>
+          <a href="/">
+            <StyledImg src={Logo} alt="Logo" />
+          </a>
+        </Box>
+
+        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+      </StyledToobar>
+    </AppBar>
   );
 };
 
