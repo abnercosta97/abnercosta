011bcb45ec8c3bd5bb7221ddfd7d318018629fd4
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2024-07-09 08:50:19 -0300

    feat: insert NavBar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
 import Hero from "./sections/Hero/Hero";
+import Navbar from "../../components/NavBar/NavBar";
 
 const Home = () => {
   return (
     <>
+      <Navbar />
       <Hero />
     </>
   );
