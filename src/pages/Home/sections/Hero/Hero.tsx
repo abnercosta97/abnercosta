b6d663e01a254f4a79aea3a28ad697a2ecd03823
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2024-07-10 20:31:25 -0300

    perf: Adicionando breakingpoint para AnimatedBackground

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,4 +1,12 @@
-import { Box, Container, Grid, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  styled,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import Avatar from "../../../../assets/images/avatar.png";
 import DownloadIcon from "@mui/icons-material/Download";
 import EmailIcon from "@mui/icons-material/Email";
@@ -7,18 +15,18 @@ import StyledButton from "../../../../components/StyledButton/StyledButton";
 import AnimatedBackground from "../../../../components/AnimatedBackground/AnimatedBacground";
 
 const Hero = () => {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
+
   const StyledHero = styled("div")(({ theme }) => ({
     backgroundColor: theme.palette.background.default,
     height: "100vh",
     display: "flex",
     alignItems: "center",
-    //position: "relative",
     [theme.breakpoints.up("xs")]: {
-      // <= mobile
       paddingTop: "350px",
     },
     [theme.breakpoints.up("md")]: {
-      // >=mobile
       paddingTop: "0",
     },
   }));
@@ -38,17 +46,19 @@ const Hero = () => {
 
   return (
     <StyledHero>
-      <Box
-        position="absolute"
-        top={0}
-        left={0}
-        width="100%"
-        height="100%"
-        zIndex={1}
-        overflow="hidden"
-      >
-        <AnimatedBackground />
-      </Box>
+      {isDesktop && (
+        <Box
+          position="absolute"
+          top={0}
+          left={0}
+          width="100%"
+          height="100%"
+          zIndex={1}
+          overflow="hidden"
+        >
+          <AnimatedBackground />
+        </Box>
+      )}
       <Container maxWidth={"lg"}>
         <Grid container spacing={2}>
           <Grid item xs={12} md={4}>
