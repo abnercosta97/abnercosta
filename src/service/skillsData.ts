7a413c2e870676ad33946f4f09155798caff7a02
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2024-07-18 22:43:56 -0300

    feat: crete section Skills e simulate api skill data

---
+++ src/service/skillsData.ts
@@ -0,0 +1,17 @@
+/*
+Simulando uma chamada de API para obter dados de habilidades
+*/
+
+const skillsData = [
+  { name: "TypeScript", icon: "/icon/typescript-icon.png" },
+  { name: "JavaScript", icon: "/icon/javascript-icon.png" },
+  { name: "HTML", icon: "/icon/html-icon.png" },
+  { name: "CSS", icon: "/icon/css-icon.png" },
+  { name: "React", icon: "/icon/react-icon.png" },
+  { name: "MongoDB", icon: "/icon/mongodb-icon.png" },
+  { name: "PostgreSQL", icon: "/icon/psql-icon.png" },
+
+  // Adicione mais habilidades conforme necessário
+];
+
+export default skillsData;
