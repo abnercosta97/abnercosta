41999da8496eb85174ad9633d32d42b5f5a4098b
abnercosta97 <abner-rodrigo@hotmail.com>
2025-02-20 09:47:22 -0300

    bug: retornando ao gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Verifica se está rodando na Vercel ou GitHub Pages
-const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
-const repoName = "abnercosta"; // Substitua pelo nome do seu repositório no GitHub
-
 export default defineConfig({
   plugins: [react()],
-  base: isGitHubPages ? `/${repoName}/` : "/", // Define base para GitHub Pages ou Vercel
+  base: "/abnercosta/", // Substitua pelo nome exato do repositório
 });
