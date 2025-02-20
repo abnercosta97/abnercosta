958347a23bf7a118a4c3fe8a1740f9116f6abc2d
abnercosta97 <abner-rodrigo@hotmail.com>
2025-02-20 09:40:30 -0300

    feat: ajustando funcionalidades para ghpages e vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Verifica se está rodando na Vercel ou GitHub Pages
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+const repoName = "abnercosta"; // Substitua pelo nome do seu repositório no GitHub
+
 export default defineConfig({
   plugins: [react()],
-  base: "/abnercosta/",
+  base: isGitHubPages ? `/${repoName}/` : "/", // Define base para GitHub Pages ou Vercel
 });
